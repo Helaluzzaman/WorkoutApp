b992f41e8a45be704210ab5a2328c6f8fae05de0
Helaluzzaman <hasib10021@gmail.com>
2021-05-06 15:06:50 +0600

    us bmii calculation added

--- app/src/main/java/com/learning/workoutapp/BmiActivity.kt
+++ app/src/main/java/com/learning/workoutapp/BmiActivity.kt
@@ -1,26 +1,33 @@
 package com.learning.workoutapp
 
-import android.icu.util.Measure
-import android.icu.util.MeasureUnit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.widget.Toolbar
-import androidx.core.widget.doOnTextChanged
+import androidx.core.view.isVisible
 import com.google.android.material.textfield.TextInputLayout
-import kotlin.math.roundToLong
 
 class BmiActivity : AppCompatActivity() {
+    private val METRIC_VIEW = "MATRIC_VIEW"
+    private val US_VIEW = "US_VIEW"
+    private var currentView = METRIC_VIEW
+
     lateinit var tb_actionbar: Toolbar
     lateinit var tl_height: TextInputLayout
     lateinit var tl_weight: TextInputLayout
     lateinit var tv_yourbmi: TextView
+    lateinit var tv_bmi: TextView
     lateinit var tv_result: TextView
     lateinit var tv_resultDescription: TextView
     lateinit var b_calculate: Button
-    private var calculateButtonHalfEnable = false
+    lateinit var ll_metricInput: LinearLayout
+    lateinit var ll_usInput: LinearLayout
+    lateinit var etWeightPd: EditText
+    lateinit var etHeightFeet: EditText
+    lateinit var etHeightInch: EditText
+    lateinit var llResult: LinearLayout
+    private var bmi = -1f
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,60 +41,123 @@ class BmiActivity : AppCompatActivity() {
             n_actionbar.setDisplayShowTitleEnabled(true)
             n_actionbar.title = "BMI calculator"
         }
+        tb_actionbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
         tl_height = findViewById(R.id.tf_height_cm)
         tl_weight = findViewById(R.id.tf_weight_kg)
         tv_yourbmi = findViewById(R.id.tv_yourBmi)
-        tv_result= findViewById(R.id.tv_bmi_result)
+        tv_bmi= findViewById(R.id.tv_bmi_value)
+        tv_result = findViewById(R.id.tv_bmi_result)
         tv_resultDescription = findViewById(R.id.tv_bmi_result_description)
         b_calculate = findViewById(R.id.b_calculate_bmi)
+        llResult = findViewById(R.id.ll_resultbmi)
         b_calculate.setOnClickListener {
-            val bmi = calculateBmi()
-            if(bmi > 0 ){
-                DisplayBmiResult(calculateBmi())
+            if(currentView == METRIC_VIEW){
+                bmi = calculateBmi()
+            }else{
+                bmi = calculateBmiUs()
             }
+            if(bmi > 0 ){
+                if(!llResult.isVisible) llResult.isVisible = true
+                DisplayBmiResult(bmi)
+            }
+        }
+        ll_metricInput = findViewById(R.id.ll_input_metric)
+        ll_usInput = findViewById(R.id.ll_input_us)
+        etWeightPd = findViewById(R.id.et_weight_pd)
+        etHeightFeet = findViewById(R.id.et_height_ft)
+        etHeightInch = findViewById(R.id.et_height_inch)
+
+    }
+    private fun calculateBmiUs(): Float{
+        val weightlb = etWeightPd.text
+        val heightFeet = etHeightFeet.text
+        val heightInch = etHeightInch.text
+        if(weightlb.isNotEmpty() && heightFeet.isNotEmpty() && heightInch.isNotEmpty()){
+            val weightkg = weightlb.toString().toFloat()/2.2046
+            val feet = heightFeet.toString().toFloat()
+            val inch = heightInch.toString().toFloat()
+            val heightm = ((feet*30.48) + (inch*2.54))/100
+            return (weightkg/(heightm*heightm)).toFloat()
+        }else {
+            Toast.makeText(this, "Enter All input!", Toast.LENGTH_SHORT).show()
+            return -1f
+        }
+    }
+
+    fun OnRadioButtonClicked(view: View){
+        if(view is RadioButton){
+            val checked = view.isChecked
+            when(view.id){
+                R.id.rb_metric -> if(checked){
+                    Toast.makeText(this, "metrix", Toast.LENGTH_SHORT).show()
+                    if(currentView == US_VIEW){
+                        ll_usInput.visibility = View.GONE
+                        ll_metricInput.isVisible = true
+                        currentView = METRIC_VIEW
+                        llResult.isVisible = false
+                    }
+                }
+                R.id.rb_us -> if(checked){
+                    Toast.makeText(this, "us", Toast.LENGTH_SHORT).show()
+                    if(currentView == METRIC_VIEW){
+                        ll_metricInput.visibility = View.GONE
+                        ll_usInput.isVisible = true
+                        currentView = US_VIEW
+                        llResult.isVisible = false
+                    }
+                }
+            }
+
         }
     }
 
     fun DisplayBmiResult(bmi: Float){
         val bmiLabel:String
-        val bmiDescription: String
+        var bmiDescription: String = ""
         when{
             bmi < 16 ->{
                 bmiLabel = "Very severely underweight"
+                bmiDescription = "Oops! You really need to take care of your better! Eat more!"
             }
             bmi < 17 -> {
                 bmiLabel = "Severely underweight"
+                bmiDescription = "Oops! You really need to take care of your better! Eat more!"
             }
             bmi < 18.5 -> {
                 bmiLabel = "Underweight"
+                bmiDescription = "Oops! You really need to take care of your better! Eat more!"
             }
             bmi < 25 -> {
                 bmiLabel = "Normal"
+                bmiDescription = "Congratulations! You are in a good shape!"
             }
             bmi < 30 -> {
                 bmiLabel = "Overweight"
+                bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
             }
             bmi < 35 ->{
                 bmiLabel = "Obese Class I"
+                bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
             }
             bmi < 40 -> {
                 bmiLabel  = "Obese Class II"
+                bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
             }
             bmi >= 40 -> {
                 bmiLabel = "Obese Class III"
+                bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
             }
             else -> {
                 bmiLabel = "unknown"
             }
         }
-
-
-
-
         tv_yourbmi.visibility = TextView.VISIBLE
-        tv_result.text = String.format("%.2f", bmi)
-        tv_resultDescription.text = bmiLabel
+        tv_bmi.text = String.format("%.2f", bmi)
+        tv_result.text = bmiLabel
+        tv_resultDescription.text = bmiDescription
     }
 
     fun calculateBmi(): Float {
@@ -100,13 +170,9 @@ class BmiActivity : AppCompatActivity() {
                 val bmi = weightkg / (heightm * heightm)
                 return bmi
             } else {
-                Toast.makeText(this, "Please Enter input First!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please Fill All input", Toast.LENGTH_SHORT).show()
             }
         }
         return -1f
     }
-
-    class BMI(
-        val bmi: Float
-    )
 }
